07a1db80480b6f067d9dd15da62550e149486ca9
mountainkuma <ty91ms@gmail.com>
2017-11-27 22:41:27 +0900

    Commit StuckQueueProject

---
+++ StackQueue/StackQueue/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace StackQueue
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtStack = new System.Windows.Forms.TextBox();
+			this.txtQueue = new System.Windows.Forms.TextBox();
+			this.btnPush = new System.Windows.Forms.Button();
+			this.btnPOP = new System.Windows.Forms.Button();
+			this.btnEnQueue = new System.Windows.Forms.Button();
+			this.btnDeQueue = new System.Windows.Forms.Button();
+			this.lblStack = new System.Windows.Forms.Label();
+			this.lblQueue = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtStack
+			// 
+			this.txtStack.Location = new System.Drawing.Point(58, 80);
+			this.txtStack.Name = "txtStack";
+			this.txtStack.Size = new System.Drawing.Size(100, 19);
+			this.txtStack.TabIndex = 0;
+			// 
+			// txtQueue
+			// 
+			this.txtQueue.Location = new System.Drawing.Point(199, 80);
+			this.txtQueue.Name = "txtQueue";
+			this.txtQueue.Size = new System.Drawing.Size(100, 19);
+			this.txtQueue.TabIndex = 1;
+			// 
+			// btnPush
+			// 
+			this.btnPush.Location = new System.Drawing.Point(58, 117);
+			this.btnPush.Name = "btnPush";
+			this.btnPush.Size = new System.Drawing.Size(75, 23);
+			this.btnPush.TabIndex = 2;
+			this.btnPush.Text = "PUSH";
+			this.btnPush.UseVisualStyleBackColor = true;
+			// 
+			// btnPOP
+			// 
+			this.btnPOP.Location = new System.Drawing.Point(58, 157);
+			this.btnPOP.Name = "btnPOP";
+			this.btnPOP.Size = new System.Drawing.Size(75, 23);
+			this.btnPOP.TabIndex = 3;
+			this.btnPOP.Text = "POP";
+			this.btnPOP.UseVisualStyleBackColor = true;
+			// 
+			// btnEnQueue
+			// 
+			this.btnEnQueue.Location = new System.Drawing.Point(199, 117);
+			this.btnEnQueue.Name = "btnEnQueue";
+			this.btnEnQueue.Size = new System.Drawing.Size(75, 23);
+			this.btnEnQueue.TabIndex = 4;
+			this.btnEnQueue.Text = "ENQUEUE";
+			this.btnEnQueue.UseVisualStyleBackColor = true;
+			// 
+			// btnDeQueue
+			// 
+			this.btnDeQueue.Location = new System.Drawing.Point(199, 157);
+			this.btnDeQueue.Name = "btnDeQueue";
+			this.btnDeQueue.Size = new System.Drawing.Size(75, 23);
+			this.btnDeQueue.TabIndex = 5;
+			this.btnDeQueue.Text = "DEQUEUE";
+			this.btnDeQueue.UseVisualStyleBackColor = true;
+			// 
+			// lblStack
+			// 
+			this.lblStack.AutoSize = true;
+			this.lblStack.Location = new System.Drawing.Point(56, 202);
+			this.lblStack.Name = "lblStack";
+			this.lblStack.Size = new System.Drawing.Size(67, 12);
+			this.lblStack.TabIndex = 6;
+			this.lblStack.Text = "StackResult";
+			// 
+			// lblQueue
+			// 
+			this.lblQueue.AutoSize = true;
+			this.lblQueue.Location = new System.Drawing.Point(197, 202);
+			this.lblQueue.Name = "lblQueue";
+			this.lblQueue.Size = new System.Drawing.Size(70, 12);
+			this.lblQueue.TabIndex = 7;
+			this.lblQueue.Text = "QueueResult";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(379, 311);
+			this.Controls.Add(this.lblQueue);
+			this.Controls.Add(this.lblStack);
+			this.Controls.Add(this.btnDeQueue);
+			this.Controls.Add(this.btnEnQueue);
+			this.Controls.Add(this.btnPOP);
+			this.Controls.Add(this.btnPush);
+			this.Controls.Add(this.txtQueue);
+			this.Controls.Add(this.txtStack);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtStack;
+		private System.Windows.Forms.TextBox txtQueue;
+		private System.Windows.Forms.Button btnPush;
+		private System.Windows.Forms.Button btnPOP;
+		private System.Windows.Forms.Button btnEnQueue;
+		private System.Windows.Forms.Button btnDeQueue;
+		private System.Windows.Forms.Label lblStack;
+		private System.Windows.Forms.Label lblQueue;
+	}
+}
+
---
+++ StackQueue/StackQueue/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StackQueue
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		#region Static
+		/// <summary>
+		/// スタック長
+		/// </summary>
+		static readonly int LENGTH_STACK = 5;
+		/// <summary>
+		/// キュー長
+		/// </summary>
+		static readonly int LENGTH_QUEUE = 5;
+		#endregion
+
+		#region Field
+		string[] stack = null;
+		string[] queue = null;
+
+		int idxStack = 0;
+		int idxEnQueue = 0;
+		int idxDeQueue = 0;
+		#endregion
+
+		#region LogicalMethod
+		/// <summary>
+		/// Push
+		/// </summary>
+		/// <param name="num"></param>
+		private void Push(string num)
+		{
+			stack[idxStack] = num;
+
+			if (idxStack < LENGTH_QUEUE - 1)
+			{
+				idxStack++;
+			}
+		}
+
+		/// <summary>
+		/// Pop
+		/// </summary>
+		/// <returns></returns>
+		private string Pop()
+		{
+			if (this.isNeededDecrementByStack())
+			{
+				idxStack--;
+			}
+
+			string buf = stack[idxStack];
+			stack[idxStack] = "";
+
+			return buf;
+		}
+
+		/// <summary>
+		/// EnQueue
+		/// </summary>
+		/// <param name="num"></param>
+		private void EnQueue(string num)
+		{
+			if (!string.IsNullOrEmpty(queue[idxEnQueue])) { return; }
+
+			queue[idxEnQueue] = num;
+
+			idxEnQueue = (idxEnQueue + 1) % LENGTH_QUEUE;
+		}
+
+		/// <summary>
+		/// DeQueue
+		/// </summary>
+		/// <returns></returns>
+		private string DeQueue()
+		{
+			if (string.IsNullOrEmpty(queue[idxDeQueue])) { return ""; }
+
+			string buf = queue[idxDeQueue];
+
+			queue[idxDeQueue] = "";
+
+			idxDeQueue = (idxDeQueue + 1) % LENGTH_QUEUE;
+
+			return buf;
+		}
+
+		#endregion
+
+		#region IOMethod
+		/// <summary>
+		/// スタック出力
+		/// </summary>
+		private void ExportStack()
+		{
+			var stackFormat = new string[LENGTH_STACK];
+
+			this.stack.CopyTo(stackFormat, 0);
+
+			// Pop対象に[]を付ける
+			if (this.stack.Any(x => x != ""))
+			{
+				if (this.isNeededDecrementByStack())
+				{
+					stackFormat[idxStack - 1] = $"[{stackFormat[idxStack - 1]}]";
+				}
+				else
+				{
+					stackFormat[idxStack] = $"[{stackFormat[idxStack]}]";
+				}
+			}
+
+			var nums = stackFormat.Select(num => $"{num}, ");
+
+			var builder = new StringBuilder();
+
+			foreach (var num in nums)
+			{
+				builder.Append(num);
+			}
+
+			this.lblStack.Text = builder.ToString();
+		}
+
+		/// <summary>
+		/// キュー出力
+		/// </summary>
+		private void ExportQueue()
+		{
+			var queueFormat = new string[LENGTH_QUEUE];
+
+			this.queue.CopyTo(queueFormat, 0);
+
+			if (this.queue.Any(x => x != ""))
+			{
+				queueFormat[idxDeQueue] = $"[{queueFormat[idxDeQueue]}]";
+			}
+
+			var nums = queueFormat.Select(num => $"{num}, ");
+
+			var builder = new StringBuilder();
+
+			foreach (var num in nums)
+			{
+				builder.Append(num);
+			}
+
+			this.lblQueue.Text = builder.ToString();
+		}
+		#endregion
+
+		#region UtillityMethod
+		/// <summary>
+		/// 数値かの確認
+		/// </summary>
+		/// <returns></returns>
+		private bool isNum(string val)
+		{
+			int pushNum = 0;
+			if (!int.TryParse(val, out pushNum)) { return false; }
+
+			return true;
+		}
+
+		/// <summary>
+		/// スタックがデクリメント必要かの確認
+		/// </summary>
+		/// <returns></returns>
+		private bool isNeededDecrementByStack()
+		{
+			if (0 < idxStack && idxStack < LENGTH_STACK - 1
+			|| idxStack == LENGTH_STACK - 1 && string.IsNullOrEmpty(this.stack[idxStack]))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// テキストボックスへのフォーカス
+		/// </summary>
+		/// <param name="txtBox"></param>
+		private void TextFocus(TextBox txtBox)
+		{
+			txtBox.Focus();
+			txtBox.SelectAll();
+		}
+		#endregion
+
+		#region Event
+		/// <summary>
+		/// ページロード処理
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Page_Load(object sender, EventArgs e)
+		{
+			this.stack = new string[LENGTH_STACK];
+			this.queue = new string[LENGTH_QUEUE];
+
+			this.stack.Select(x => "");
+			this.queue.Select(x => "");
+		}
+
+		/// <summary>
+		/// Pushボタン押下イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnPush_Click(object sender, EventArgs e)
+		{
+			if (!this.isNum(this.txtStack.Text)) { return; }
+
+			this.Push(this.txtStack.Text);
+
+			this.ExportStack();
+
+			this.TextFocus(txtStack);
+		}
+
+		/// <summary>
+		/// Popボタン押下イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnPop_Click(object sender, EventArgs e)
+		{
+			this.txtStack.Text = this.Pop();
+
+			this.ExportStack();
+
+			this.TextFocus(txtStack);
+		}
+
+		/// <summary>
+		/// EnQueueボタン押下イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnEnQueue_Click(object sender, EventArgs e)
+		{
+			if (!this.isNum(this.txtQueue.Text)) { return; }
+
+			this.EnQueue(this.txtQueue.Text);
+
+			this.ExportQueue();
+
+			this.TextFocus(txtQueue);
+		}
+
+		/// <summary>
+		/// DeQueueボタン押下イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnDeQueue_Click(object sender, EventArgs e)
+		{
+			this.txtQueue.Text = this.DeQueue();
+
+			this.ExportQueue();
+
+			this.TextFocus(txtQueue);
+		}
+
+		/// <summary>
+		/// テキストボックス押下イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void txtStack_Click(object sender, EventArgs e)
+		{
+			var txtBox = (TextBox)sender;
+
+			txtBox.SelectAll();
+		}
+		#endregion
+	}
+}
